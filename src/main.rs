e4b1b69e7417307e794c7f2c0663f482e8347063
kgalang <kevingalang@protonmail.com>
2020-05-24 20:57:30 -0400

    Read file into lines to apply analyzer.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+
+struct DiffString;
+
+impl DiffString {
+    fn from_file(path: &str) -> Result<String, std::io::Error> {
+        let mut input = File::open(path)?;
+        let mut input_buffer = String::new();
+        input.read_to_string(&mut input_buffer)?;
+        Ok(input_buffer)
+    }
+    // fn from_github
+}
+
+fn main() -> std::io::Result<()> {
+    let diff = DiffString::from_file("./fixtures/edit-ws.diff")?;
+
+    let mut lines = diff.lines();
+
+    println!("Lines {:?}", lines.next());
+    Ok(())
 }
