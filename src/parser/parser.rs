a6988e4c08160267afbbcf87091690486d5149f5
kgalang <kevingalang@protonmail.com>
2020-05-25 17:35:12 -0400

    Write psuedo code and create DiffLines while iterating.

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -6,18 +6,49 @@ lazy_static! {
   static ref HUNK_HEADER: Regex = Regex::new(r"(^@@)").unwrap();
 }
 
+pub fn create_line(line: String) -> DiffLine {
+  // line actions
+  let insert_prefix = "+";
+  let delete_prefix = "-";
+
+  let mut action = DiffLineActions::Context;
+  if line.starts_with(insert_prefix) {
+    action = DiffLineActions::Insert;
+  } else if line.starts_with(delete_prefix) {
+    action = DiffLineActions::Delete
+  }
+
+  DiffLine {
+    action: action,
+    content: line
+  }
+}
+
 pub fn parse(diff: String) -> Vec<DiffFile>{
   // iterate through each line
+  // if file start
+  //    save previous file to out_files vec and create new struct file
+  // if hunk header
+  //    add to DiffFile hunk_header
+  //    save previous hunk to cur file, then start new hunk
+  // else
+  //    create line and add to hunk
+
   let mut out_files: Vec<DiffFile> = Vec::new();  
   let mut i = 0;
+  let mut lines = diff.lines();
 
-  for line in diff.lines() {
+  for line in lines {
     i += 1;
 
     if let Some(caps) = DIFF_FILE_START.captures(line) {
-      println!("{} DIFF START: {}", i, line)
+      println!("{} DIFF START: {}", i, line);
+      // println!("DIFF START + 1: {}", line.next());
     } else if let Some(caps) = HUNK_HEADER.captures(line) {
       println!("{} HUNK HEADER: {}", i, line)
+    } else {
+      let diff_line = create_line(line.to_string());
+      println!("{} {:?}", i, diff_line)
     }
   }
   
@@ -33,7 +64,6 @@ pub fn parse(diff: String) -> Vec<DiffFile>{
     is_combined: false,
     lang: Languages::Python,
     hunks: None,
-    hunk_header: None,
   };
   
   vec![ex_file]
