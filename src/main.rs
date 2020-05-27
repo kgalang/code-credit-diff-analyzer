ececc5fc8eebadfb0e569179f81be4e25c663f27
kgalang <kevingalang@protonmail.com>
2020-05-26 20:53:28 -0400

    get file extension from patched files.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
+use unidiff::{PatchSet, PatchedFile, Hunk};
+use std::path::Path;
 
 struct DiffString;
 
@@ -13,20 +15,38 @@ impl DiffString {
     // fn from_github
 }
 
-fn main() -> std::io::Result<()> {
-    use unidiff::PatchSet;
+#[derive(Debug)]
+pub struct HunkStats {
+    pub lang: String,
+    pub mode: String,
+    pub added_ct: i32,
+    pub removed_ct: i32,
+    pub trimmed_added_ct: i32,
+    pub trimmed_removed_ct: i32,
+}
 
+#[derive(Debug)]
+pub struct DiffStats(pub Vec<HunkStats>);
+
+fn get_hunk_stats(file: &PatchedFile, hunk: &Hunk) {
+    let f_ext = Path::new(file.target_file.as_str()).extension().unwrap();
+    println!("{:?}", f_ext);
+}
+
+fn main() -> std::io::Result<()> {
     let diff = DiffString::from_file("./fixtures/rustball.diff")?;
 
-    // let out = parse(diff);
     let mut patch = PatchSet::new();
     patch.parse(diff).ok().expect("Error parsing diff");
-
-    let mut patchset_iter = patch.into_iter();
-    let mut f = patchset_iter.nth(0).unwrap();
-    let mut h = f.into_iter().nth(0).unwrap();
-    println!("{:?}", h);
-    println!("{:?}", h.into_iter().nth(0).unwrap());
+    let files = patch.files();
+    let mut out_stats: Vec<HunkStats> = Vec::new();
+    
+    for f in files {
+        println!("f here ------- {:?}", f);
+        for h in f.hunks() {
+            get_hunk_stats(f, h);
+        }
+    }
 
     Ok(())
 }
