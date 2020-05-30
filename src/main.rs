ddd6112159839383d6e31f243bbaa13d77232724
kgalang <kevingalang@protonmail.com>
2020-05-30 13:51:32 -0400

    extract models into separate file.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
 #[macro_use]
 extern crate lazy_static;
 
-use std::fs::File;
-use std::io::prelude::*;
+mod models;
+
 use unidiff::{PatchSet, PatchedFile, Hunk, Line};
 use std::path::Path;
 use std::collections::HashMap;
+use models::{Language, HunkStats, DiffString};
 
-#[derive(PartialEq, Eq, Hash, Clone, Debug)]
-pub enum Language {
-    Other,
-    Python,
-    Ruby,
-    Rust,
-    Javascript,
-}
 
 lazy_static!{
     static ref LANG_EXT: HashMap<&'static str, Language> = {
@@ -34,32 +27,6 @@ lazy_static!{
         map
     };
 }
-
-struct DiffString;
-
-impl DiffString {
-    fn from_file(path: &str) -> Result<String, std::io::Error> {
-        let mut input = File::open(path)?;
-        let mut input_buffer = String::new();
-        input.read_to_string(&mut input_buffer)?;
-        Ok(input_buffer)
-    }
-    // fn from_github
-}
-
-#[derive(Debug)]
-pub struct HunkStats {
-    pub lang: Language,
-    pub raw_added: usize,
-    pub raw_removed: usize,
-    pub raw_lines: Vec<Line>,
-    pub cleaned_added: usize,
-    pub cleaned_removed: usize,
-    pub cleaned_lines: Vec<Line>,
-}
-
-#[derive(Debug)]
-pub struct DiffStats(pub Vec<HunkStats>);
 
 fn is_not_comment(line: &Line, lang: &Language) -> bool {
     if let Language::Other = lang {
---
+++ src/models.rs
@@ -0,0 +1,36 @@
+use unidiff::{Line};
+use std::io::prelude::*;
+use std::fs::File;
+
+
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+pub enum Language {
+    Other,
+    Python,
+    Ruby,
+    Rust,
+    Javascript,
+}
+
+pub struct DiffString;
+
+impl DiffString {
+    pub fn from_file(path: &str) -> Result<String, std::io::Error> {
+        let mut input = File::open(path)?;
+        let mut input_buffer = String::new();
+        input.read_to_string(&mut input_buffer)?;
+        Ok(input_buffer)
+    }
+    // fn from_github
+}
+
+#[derive(Debug)]
+pub struct HunkStats {
+    pub lang: Language,
+    pub raw_added: usize,
+    pub raw_removed: usize,
+    pub raw_lines: Vec<Line>,
+    pub cleaned_added: usize,
+    pub cleaned_removed: usize,
+    pub cleaned_lines: Vec<Line>,
+}
